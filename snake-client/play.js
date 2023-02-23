7a81b8e4b11b5645f57da5e0c9154d2ab22267d0
Tony Tran <121963221+tonytran1997@users.noreply.github.com>
2023-02-22 19:46:07 -0500

    Add files via upload

---
+++ snake-client/play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '172.24.150.191',  // IP address here,
+    port: 50541, // PORT number here,
+  });
+
+  conn.on("connect", (connect) => {
+    // code that does something when the connection is first established
+    console.log('Connected to server!')
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
