2e1b8fd081bc20ec5daa63090bb80c8c93391e1f
Tony Tran <121963221+tonytran1997@users.noreply.github.com>
2023-02-22 20:19:42 -0500

    Add files via upload

--- snake-client/client.js
+++ snake-client/client.js
@@ -1,7 +1,5 @@
 const net = require("net");
-const connect = require('./client');
 
-/* establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: '172.24.150.191',  // IP address here,
@@ -22,4 +20,4 @@ const connect = function () {
 console.log("Connecting ...");
 connect();
 
-module.exports = connect;*/
+module.exports = connect;
