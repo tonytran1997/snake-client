d786e1f1f89257c96b8d753154723904b578f3f7
Tony Tran <121963221+tonytran1997@users.noreply.github.com>
2023-02-24 00:47:15 -0500

    Add files via upload

--- snake-client/input.js
+++ snake-client/input.js
@@ -1,13 +1,32 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
 
-  const handleUserInput = function () {
+  const handleUserInput = function (key) {
     // your code here
+    if (key === MOVE_UP_KEY) {
+      conn.write('Move: up');
+    }
+    if (key === MOVE_LEFT_KEY) {
+      conn.write('Move: left');
+    }
+    if (key === MOVE_DOWN_KEY) {
+      conn.write('Move: down');
+    } 
+    if (key === MOVE_RIGHT_KEY) {
+      conn.write('Move: right');
+    }
   };
-
   return stdin;
 };
+
+const messages = {
+  '1': 'Say: You are mine!',
+  '2': 'Say: This is too easy...'
+}
